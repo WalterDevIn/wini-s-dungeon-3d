9eafb16c47cd9b4953bf53231b422ac29d00b76a
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-04 02:29:48 -0300

    Coordinate hito 4 guest character flow

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,25 @@
 import { connectToServer } from './net/socketClient.js';
+import { createCharacterSelectScreen } from './screens/characterSelectScreen.js';
 import { createMenuScreen } from './screens/menuScreen.js';
 import { renderScreen } from './ui/screenRouter.js';
 
 const app = document.querySelector('#app');
-const menuScreen = createMenuScreen();
+let socketClient = null;
+
+const menuScreen = createMenuScreen({
+  onEnterAsGuest(displayName) {
+    socketClient?.enterAsGuest(displayName);
+  },
+});
 
 renderScreen(app, menuScreen.element);
 
-connectToServer({
+socketClient = connectToServer({
   onSessionCreated(sessionId) {
     menuScreen.setConnectedSession(sessionId);
+  },
+  onCharacterReady(character) {
+    renderScreen(app, createCharacterSelectScreen(character));
   },
   onDisconnect() {
     menuScreen.setDisconnected();
