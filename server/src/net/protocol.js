8548b76df5fe956ea3a460d785f5660c0785ba52
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-04 03:34:53 -0300

    Add hito 6 socket protocol events

--- server/src/net/protocol.js
+++ server/src/net/protocol.js
@@ -1,11 +1,14 @@
 const clientEvents = Object.freeze({
   enterAsGuest: 'ENTER_AS_GUEST',
+  joinDungeon: 'JOIN_DUNGEON',
 });
 
 const serverEvents = Object.freeze({
   sessionCreated: 'SESSION_CREATED',
   guestReady: 'GUEST_READY',
   characterReady: 'CHARACTER_READY',
+  dungeonJoined: 'DUNGEON_JOINED',
+  worldSnapshot: 'WORLD_SNAPSHOT',
 });
 
 module.exports = {
