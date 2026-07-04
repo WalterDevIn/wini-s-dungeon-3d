f2360813fe648b4e831a7a83d9d572ba4349a5ac
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-04 02:17:54 -0300

    Connect client to hito 3 socket server

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,17 @@
+import { connectToServer } from './net/socketClient.js';
 import { createMenuScreen } from './screens/menuScreen.js';
 import { renderScreen } from './ui/screenRouter.js';
 
 const app = document.querySelector('#app');
+const menuScreen = createMenuScreen();
 
-renderScreen(app, createMenuScreen());
+renderScreen(app, menuScreen.element);
+
+connectToServer({
+  onSessionCreated(sessionId) {
+    menuScreen.setConnectedSession(sessionId);
+  },
+  onDisconnect() {
+    menuScreen.setDisconnected();
+  },
+});
