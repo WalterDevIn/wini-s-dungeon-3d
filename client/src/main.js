221f19f01a34a5d8b39aa9e62dae06dd9deaf6aa
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-04 03:06:20 -0300

    Open hito 5 local 3d room

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,18 @@
 import { connectToServer } from './net/socketClient.js';
+import { createGameScreen } from './screens/gameScreen.js';
 import { createCharacterSelectScreen } from './screens/characterSelectScreen.js';
 import { createMenuScreen } from './screens/menuScreen.js';
 import { renderScreen } from './ui/screenRouter.js';
 
 const app = document.querySelector('#app');
 let socketClient = null;
+let activeScreen = null;
+
+function mountScreen(screen) {
+  activeScreen?.dispose?.();
+  activeScreen = screen;
+  renderScreen(app, screen.element || screen);
+}
 
 const menuScreen = createMenuScreen({
   onEnterAsGuest(displayName) {
@@ -12,14 +20,18 @@ const menuScreen = createMenuScreen({
   },
 });
 
-renderScreen(app, menuScreen.element);
+mountScreen(menuScreen);
 
 socketClient = connectToServer({
   onSessionCreated(sessionId) {
     menuScreen.setConnectedSession(sessionId);
   },
   onCharacterReady(character) {
-    renderScreen(app, createCharacterSelectScreen(character));
+    mountScreen(createCharacterSelectScreen(character, {
+      onEnterDungeon() {
+        mountScreen(createGameScreen());
+      },
+    }));
   },
   onDisconnect() {
     menuScreen.setDisconnected();
