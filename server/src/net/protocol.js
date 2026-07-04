2b8919907e64f37c398355921bcefc7c643ab8ff
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-04 02:29:04 -0300

    Extend hito 4 socket protocol

--- server/src/net/protocol.js
+++ server/src/net/protocol.js
@@ -1,7 +1,14 @@
+const clientEvents = Object.freeze({
+  enterAsGuest: 'ENTER_AS_GUEST',
+});
+
 const serverEvents = Object.freeze({
   sessionCreated: 'SESSION_CREATED',
+  guestReady: 'GUEST_READY',
+  characterReady: 'CHARACTER_READY',
 });
 
 module.exports = {
+  clientEvents,
   serverEvents,
 };
