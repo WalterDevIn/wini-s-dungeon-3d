559e77d266d67a4d73693ad289cb80dafb181d71
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-04 02:29:12 -0300

    Handle hito 4 enter as guest

--- server/src/net/socketServer.js
+++ server/src/net/socketServer.js
@@ -1,6 +1,13 @@
 const { randomUUID } = require('node:crypto');
 const { Server } = require('socket.io');
-const { serverEvents } = require('./protocol');
+const { createAccountService } = require('../auth/accountService');
+const { createSessionService } = require('../auth/sessionService');
+const { createCharacterService } = require('../characters/characterService');
+const { clientEvents, serverEvents } = require('./protocol');
+
+const accountService = createAccountService();
+const characterService = createCharacterService();
+const sessionService = createSessionService();
 
 function attachSocketServer(httpServer) {
   const io = new Server(httpServer, {
@@ -12,9 +19,27 @@ function attachSocketServer(httpServer) {
   io.on('connection', (socket) => {
     const sessionId = randomUUID();
 
+    sessionService.createSession({ sessionId, socketId: socket.id });
+
     console.log(`Client connected: ${sessionId}`);
 
     socket.emit(serverEvents.sessionCreated, { sessionId });
+
+    socket.on(clientEvents.enterAsGuest, ({ displayName } = {}) => {
+      console.log(`ENTER_AS_GUEST received: ${sessionId}`);
+
+      const account = accountService.createGuestAccount(displayName);
+      sessionService.attachGuestAccount(sessionId, account);
+
+      const character = characterService.getOrCreateDefaultCharacter(account);
+
+      socket.emit(serverEvents.guestReady, { account });
+      socket.emit(serverEvents.characterReady, { character });
+    });
+
+    socket.on('disconnect', () => {
+      sessionService.removeSession(sessionId);
+    });
   });
 
   return io;
