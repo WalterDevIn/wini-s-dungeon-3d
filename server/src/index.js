549fd1bb4ce68f03463a5f59b2e6473aa8203062
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-04 02:17:39 -0300

    Attach hito 3 socket server

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 const http = require('node:http');
+const { attachSocketServer } = require('./net/socketServer');
 
 const port = Number(process.env.PORT) || 3000;
 
@@ -6,6 +7,8 @@ const server = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
   response.end('Wini-s-dungeon-3d server running');
 });
+
+attachSocketServer(server);
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
