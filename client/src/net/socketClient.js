752a33565c4e158c14267fb9d97b7e60f2e45ae0
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-04 02:29:18 -0300

    Support hito 4 guest socket flow

--- client/src/net/socketClient.js
+++ client/src/net/socketClient.js
@@ -1,14 +1,26 @@
 import { io } from 'socket.io-client';
 
+const clientEvents = Object.freeze({
+  enterAsGuest: 'ENTER_AS_GUEST',
+});
+
 const serverEvents = Object.freeze({
   sessionCreated: 'SESSION_CREATED',
+  guestReady: 'GUEST_READY',
+  characterReady: 'CHARACTER_READY',
 });
 
 function getServerUrl() {
   return import.meta.env.VITE_SERVER_URL || 'http://localhost:3000';
 }
 
-export function connectToServer({ onConnect, onSessionCreated, onDisconnect } = {}) {
+export function connectToServer({
+  onConnect,
+  onSessionCreated,
+  onGuestReady,
+  onCharacterReady,
+  onDisconnect,
+} = {}) {
   const socket = io(getServerUrl());
 
   socket.on('connect', () => {
@@ -19,9 +31,21 @@ export function connectToServer({ onConnect, onSessionCreated, onDisconnect } = 
     onSessionCreated?.(sessionId);
   });
 
+  socket.on(serverEvents.guestReady, ({ account }) => {
+    onGuestReady?.(account);
+  });
+
+  socket.on(serverEvents.characterReady, ({ character }) => {
+    onCharacterReady?.(character);
+  });
+
   socket.on('disconnect', () => {
     onDisconnect?.();
   });
 
-  return socket;
+  return {
+    enterAsGuest(displayName) {
+      socket.emit(clientEvents.enterAsGuest, { displayName });
+    },
+  };
 }
